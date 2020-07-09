e323eaab36afea18bc4441968ce695ebc229ec02
Yesme <moralesyesme@gmail.com>
2020-07-09 16:14:53 -0500

    Updates

---
+++ precache-manifest.ac5b053f5a262fa8735ffd8d2361bdae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e237309b81dabe0d48d91dfe4a225486",
+    "url": "/rick-and-morty-react/index.html"
+  },
+  {
+    "revision": "fc83ff8c9b08aa68ea64",
+    "url": "/rick-and-morty-react/static/css/main.08d4ce63.chunk.css"
+  },
+  {
+    "revision": "0e2fab427d7f04850d36",
+    "url": "/rick-and-morty-react/static/js/2.54bc86c5.chunk.js"
+  },
+  {
+    "revision": "a9dd647aec81fffade4fa7e6d693dad7",
+    "url": "/rick-and-morty-react/static/js/2.54bc86c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc83ff8c9b08aa68ea64",
+    "url": "/rick-and-morty-react/static/js/main.9565be43.chunk.js"
+  },
+  {
+    "revision": "4782415dcb347d8db047",
+    "url": "/rick-and-morty-react/static/js/runtime-main.84ea4004.js"
+  }
+]);
